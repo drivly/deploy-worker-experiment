8c508a49a6ef07ca92483801bf7c8b85ee368c6f
Nathan Clevenger <nateclev@gmail.com>
2022-09-26 01:45:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ import toml from 'toml'
 
 try {
   const accountId = core.getInput('accountId')
-  const name = core.getInput('name')
-  const environment = core.getInput('environment')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = (new Date()).toTimeString()
 
-  fetch(`https://dash.cloudflare.com/api/v4/accounts/${accountId}/workers/services/${name}/environments/${environment}?include_subdomain_availability=true`, {
-    "headers": {
-      "accept": "application/json",
-      "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryWnu1tPSa9hrmPCdJ",
-    },
-    "body": "------WebKitFormBoundaryWnu1tPSa9hrmPCdJ\r\nContent-Disposition: form-data; name=\"worker.js\"; filename=\"worker.js\"\r\nContent-Type: application/javascript+module\r\n\r\nexport default {\n  fetch: req => new Response(JSON.stringify(req.cf, undefined, 2))\n}\r\n------WebKitFormBoundaryWnu1tPSa9hrmPCdJ\r\nContent-Disposition: form-data; name=\"metadata\"; filename=\"blob\"\r\nContent-Type: application/json\r\n\r\n{\"bindings\":[],\"main_module\":\"worker.js\"}\r\n------WebKitFormBoundaryWnu1tPSa9hrmPCdJ--\r\n",
-    "method": "PUT"
-  })
+//   fetch(`https://dash.cloudflare.com/api/v4/accounts/${accountId}/workers/services/${name}/environments/${environment}?include_subdomain_availability=true`, {
+//     "headers": {
+//       "accept": "application/json",
+//       "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryWnu1tPSa9hrmPCdJ",
+//     },
+//     "body": "------WebKitFormBoundaryWnu1tPSa9hrmPCdJ\r\nContent-Disposition: form-data; name=\"worker.js\"; filename=\"worker.js\"\r\nContent-Type: application/javascript+module\r\n\r\nexport default {\n  fetch: req => new Response(JSON.stringify(req.cf, undefined, 2))\n}\r\n------WebKitFormBoundaryWnu1tPSa9hrmPCdJ\r\nContent-Disposition: form-data; name=\"metadata\"; filename=\"blob\"\r\nContent-Type: application/json\r\n\r\n{\"bindings\":[],\"main_module\":\"worker.js\"}\r\n------WebKitFormBoundaryWnu1tPSa9hrmPCdJ--\r\n",
+//     "method": "PUT"
+//   })
 
 
   core.setOutput("time", time);
