aae5157428df1f227b066d19fb921bcc08e96524
Nathan Clevenger <nateclev@gmail.com>
2022-09-26 02:12:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ try {
   const wrangler = toml.parse(fs.readFileSync('wrangler.toml'))
   const worker = fs.readFileSync(wrangler.main)
   
-  const bundle = esbuild.transform(worker)
+  const bundle = esbuild.transform(wrangler.main)
   console.log(bundle)
 //   esbuild.transform(code, options).then(result => { ... })
 //   esbuild.build(options).then(result => { ... })
