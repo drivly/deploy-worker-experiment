45969707b061336ce5934807f7bc02e5ad4cfa44
Nathan Clevenger <nateclev@gmail.com>
2022-09-26 01:42:41 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,5 @@
+import { zip } from 'https://unpkg.com/lodash-es@4.17.15/lodash.js'
+
 export default {
-  fetch: req => new Response(JSON.stringify({ hello: 'world' }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  fetch: req => new Response(JSON.stringify({ hello: 'world', zip: zip([1, 2], ['a', 'b']) }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
 }
