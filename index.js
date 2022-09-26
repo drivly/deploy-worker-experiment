98e9046d8f8dfe0af0ab500bd1c0c918dc50819b
Nathan Clevenger <nateclev@gmail.com>
2022-09-26 01:50:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
 //   })
 
 
-  core.setOutput("time", time);
+//   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
